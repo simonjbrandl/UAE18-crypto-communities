3f0bc15d1fceaf2fd35ef75d36aa8242643efed2
simonjbrandl <simonjbrandl@gmail.com>
2019-10-24 21:27:44 +0200

    Started Outline and created figure pngs

--- presentation_theme.R
+++ presentation_theme.R
@@ -9,3 +9,16 @@ theme_presentation <- function (base_size = 11, base_family = "") {
           axis.text = element_text(color = "white"),
           axis.title = element_text(color = "white"))
 }
+
+
+theme_sjb <- function (base_size = 8, base_family = "") {
+  theme_bw() %+replace% 
+    theme(panel.background = element_blank(),
+          panel.border = element_blank(),
+          axis.line = element_line(color = "black"),
+          axis.ticks = element_line(color = "black"),
+          axis.text = element_text(color = "black", size = 8, family = "Arial"),
+          axis.title = element_text(color = "black", size = 8, family = "Arial"),
+          legend.title=element_text(size=8), 
+          legend.text=element_text(size=8))
+}
