d35bfc5b3e4cb8d4a0f7ada65cb8472ec1f547a2
simonjbrandl <simonjbrandl@gmail.com>
2019-10-24 17:48:26 +0200

    Added network plots, changed lots of little bits

---
+++ presentation_theme.R
@@ -0,0 +1,11 @@
+theme_presentation <- function (base_size = 11, base_family = "") {
+  theme_bw() %+replace% 
+    theme(panel.grid.major  = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank(),
+          panel.border = element_blank(),
+          axis.line = element_line(color = "white"),
+          axis.ticks = element_line(color = "white"),
+          axis.text = element_text(color = "white"),
+          axis.title = element_text(color = "white"))
+}
